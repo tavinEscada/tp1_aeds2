5e42d69c1a12b1399e5aa6366187961e3866a637
sakura <beatriz.cruz.silva@ufv.br>
2025-06-10 20:30:01 -0300

    modificaçoes na hash

--- include/hash.h
+++ include/hash.h
@@ -3,7 +3,42 @@
 
 #include <string.h>
 #include<stdlib.h>
+#include<stdio.h>
 #include <sys/time.h>
+#include "Palavras.h"
+
+#define M 7 //tamanho da tabela mudar de acordo com o Tp
+#define N 50 // tamanho para a maior palavrea em ingles e portugues
+#define TAMALFABETO 256 // num possivel de caracters ASCII
+
+/* typedef unsigned int  TipoPesos[n]; */
+typedef char Tipopalavra[N]; //palavra
+typedef unsigned TipoPesos[N][TAMALFABETO]; //nao sei pq esta assim
+typedef struct TipoItem {
+    Tipopalavra palavra;
+	Ccelula *indice;
+]} TipoItem; // pesquisar
+typedef unsigned int TipoIndice;
+typedef struct TipoCelula* TipoApontador;
+typedef struct TipoCelula {
+    TipoItem Item; //palavra e outros componentes
+    TipoApontador Prox;
+} TipoCelula; //lista encadeada
+typedef struct TipoLista {
+    TipoCelula *Primeiro, *Ultimo;
+} TipoLista;
+typedef TipoLista TipoDicionario[M];//nossa tabela(vetor de tipolista)
+TipoDicionario Tabela;
+TipoItem Elemento;
+TipoPesos p;
+TipoApontador i;
 
 
+void Inicializa(TipoDicionario T);
+
+void FLVazia(TipoLista *Lista);
+
+void GeraPesos(TipoPesos p);
+
+TipoIndice h(Tipopalavra palavra, TipoPesos p);
 #endif
--- src/hash.c
+++ src/hash.c
@@ -1,1 +1,31 @@
 #include "../include/hash.h"
+
+
+
+void FLVazia(TipoLista *Lista){
+	Lista->Primeiro = (TipoCelula *)malloc(sizeof(TipoCelula));
+  	Lista->Ultimo = Lista->Primeiro; Lista->Primeiro->Prox = NULL;
+}
+void Inicializa(TipoDicionario T){
+	int i;
+  	for (i = 0; i < M; i++) FLVazia(&T[i]);
+}
+
+void GeraPesos(TipoPesos p)
+{ /* Gera valores randomicos entre 1 e 10.000 */
+  int i, j;
+  struct timeval semente;
+  /* Utilizar o tempo como semente para a funcao srand() */
+  gettimeofday(&semente, NULL);
+  srand((int)(semente.tv_sec + 1000000 * semente.tv_usec));
+  for (i = 0; i < N; i++)
+    for (j = 0; j < TAMALFABETO; j++)
+      p[i][j] = 1 + (int)(10000.0 * rand() / (RAND_MAX + 1.0));
+}
+
+TipoIndice h(Tipopalavra palavra, TipoPesos p)
+{ int i; unsigned int Soma = 0;
+  int comp = strlen(palavra);
+  for (i = 0; i < comp; i++) Soma += p[i][(unsigned int)palavra[i]];
+  return (Soma % M);
+}
