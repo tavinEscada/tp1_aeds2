62becd23405b2d725bf684c59555c6f9625f1625
heitor porto <hectorpovov@gmail.com>
2025-06-10 20:25:39 -0300

    tad palavra completo

--- include/Palavra.h
+++ include/Palavra.h
@@ -21,8 +21,8 @@ typedef struct TipoItem
 }TipoItem;
 
 void faz_palavra_vazia( TipoItem * item, char * palavra);
-int imprime_indice_invertido( TipoItem * item);
-int insere_palavra (TipoItem * item, int idDoc);
+void imprime_indice_invertido( TipoItem * item);
+void insere_palavra (TipoItem * item, int idDoc);
 int remove_palavra (TipoItem * item, int idDoc);
 
 
